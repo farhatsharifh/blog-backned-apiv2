e8f62de94330b802a3a459e383d9bdf291c32edd
Farhat Sharif <farhatsharif.it@gmail.com>
2020-10-15 20:36:26 +0500

    setting up project to deploy at DO

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from './app'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-    console.log('Running on Port 3000.....');
+    console.log(`Running on Port ${port} .....`);
 });
