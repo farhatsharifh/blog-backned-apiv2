388982034595e67e25fc29a31fabf2928010e8a8
Farhat Sharif <farhatsharif.it@gmail.com>
2020-10-17 02:06:41 +0500

    configures dev and prod environments

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from './app'
 
-const port = process.env.PORT || 3001;
+const PORT = 3001;
 
-app.listen(port, () => {
-    console.log(`Running on Port ${port} .....`);
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT} .....`);
 });
